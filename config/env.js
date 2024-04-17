392837566d6a16cfdc6f2acc0d660bf4621d4d8a
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2024-04-17 12:24:04 +0200

    Schemas

---
+++ config/env.js
@@ -0,0 +1,24 @@
+import "dotenv/config"
+
+const {
+    UPLOADS_DIR,
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+    RESEND_API_KEY,
+    SECRET,
+} = process.env
+
+const { PORT } = process.env || 3000
+
+export {
+    UPLOADS_DIR,
+    PORT,
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+    RESEND_API_KEY,
+    SECRET,
+}
