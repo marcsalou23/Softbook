7b7ab3f1e5f0e29c0a89c2540b77cacd001d346e
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2024-04-20 09:42:05 +0200

    Añadiendo utils + services

---
+++ services/propertiesServices.js
@@ -0,0 +1,35 @@
+const APIUrl = import.meta.env.VITE_API_URL
+
+// Ver todas las propiedades
+const getAllPropertiesService = async () => {
+    const res = await fetch(`${APIUrl}/properties`)
+
+    const body = await res.json()
+
+    return body
+}
+
+// Ver todas las propiedades filtradas
+const getAllFilterPropertiesService = async (params) => {
+    const res = await fetch(`${APIUrl}/properties?${params}`)
+    if (res.status !== 200) {
+        return
+    }
+    const body = await res.json()
+
+    return body
+}
+
+// Ver una propiedad en detalle
+const getPropertyDetailsService = async (id) => {
+    const res = await fetch(`${APIUrl}/properties/${id}`)
+    const body = await res.json()
+
+    return body
+}
+
+export {
+    getAllPropertiesService,
+    getAllFilterPropertiesService,
+    getPropertyDetailsService,
+}
