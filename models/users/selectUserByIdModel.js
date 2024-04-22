625621813024b2162abcb917b9ab88dc2e946bd4
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2024-04-22 11:21:14 +0200

    Fix Models

--- models/users/selectUserByIdModel.js
+++ models/users/selectUserByIdModel.js
@@ -6,7 +6,14 @@ const selectUserByIdModel = async (id) => {
         connection = await getDb()
 
         let [users] = await connection.query(
-            `SELECT * FROM users WHERE id = ?`,
+            `SELECT *,
+            ROUND((SELECT AVG(r.rating)
+              FROM reviews r, bookings b, properties p
+              WHERE p.owner_id = u.id
+              AND p.id = b.property_id
+              AND b.id = r.booking_id), 1) AS media_rating
+            FROM users u 
+               WHERE u.id = ?`,
             [id]
         )
         return users
