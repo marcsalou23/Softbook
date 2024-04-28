e8eea300d8abe723535e983186d193346ba0d3d4
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2024-04-28 10:52:38 +0200

    Integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { Routes, Route } from "react-router-dom"
 
 // Importación de páginas
 import HomePage from "./pages/HomePage"
-import ActiveRentingsPage from "./pages/ActiveRentingsPage"
 import PendingRequestsPage from "./pages/PendingRequestsPage"
 import PendingReviewsPage from "./pages/PendingReviewsPage"
 import PropertyDetailsPage from "./pages/PropertyDetailsPage"
 import ViewUserProfilePage from "./pages/ViewUserProfilePage"
 import NotFoundPage from "./pages/NotFoundPage"
-
+import ValidateUserPage from "./pages/ValidateUserPage"
 // Importación de componentes
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import PrivateRoutes from "./components/PrivateRoutes"
 
 function App() {
     return (
@@ -24,21 +24,23 @@ function App() {
                     path="/properties/:id"
                     element={<PropertyDetailsPage />}
                 />
-                {/* Rutas privadas */}
-                <Route path="/profile/" element={<ViewUserProfilePage />} />
                 <Route
-                    path="/profile/rentings"
-                    element={<ActiveRentingsPage />}
-                />
-                <Route
-                    path="/profile/reviews"
-                    element={<PendingReviewsPage />}
-                />
-                <Route
-                    path="/profile/requests"
-                    element={<PendingRequestsPage />}
+                    path="/users/validate/:registration_code"
+                    element={<ValidateUserPage />}
                 />
                 <Route path="*" element={<NotFoundPage />} />
+                {/* Rutas privadas */}
+                <Route element={<PrivateRoutes />}>
+                    <Route path="/profile/" element={<ViewUserProfilePage />} />
+                    <Route
+                        path="/profile/reviews"
+                        element={<PendingReviewsPage />}
+                    />
+                    <Route
+                        path="/profile/requests"
+                        element={<PendingRequestsPage />}
+                    />
+                </Route>
             </Routes>
             <Footer />
         </>
